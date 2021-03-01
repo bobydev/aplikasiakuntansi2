dc96d8c937db51b459e7b64a6cd287d526d03714
Boby Aprespa <61673107+bobydev@users.noreply.github.com>
2021-03-01 13:44:59 +0700

    Revert "Boby"
    This reverts commit 742a805fe327a300c7f11a42fe5ace7239ae9e5c.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
     }
 }
--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -13,14 +12,5 @@ class RoleSeeder extends Seeder
     public function run()
     {
         //
-        Role::create([
-            'name' => 'admin',
-            'guard_name' => 'web'
-        ]);
-
-        Role::create([
-            'name' => 'user',
-            'guard_name' => 'web'
-        ]);
     }
 }
